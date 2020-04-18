e579ed738f9bcfd245db28d7412285577b0267a1
Osama Mustafa <O.mustafaoracle@yahoo.com>
2020-04-18 16:53:13 +0300

    Just example to show you how to use function
    Just example to show you how to use function

---
+++ simple-fucntion-example.py
@@ -0,0 +1,12 @@
+def subfunction():
+    print("Lets call the subfunction:")
+
+def main():
+    subfunction()
+    print("Hello there!")
+    print("Quitting.")
+
+
+
+if __name__ == "__main__":
+    main()
