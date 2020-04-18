5bf8ba6972b3cc3de89f0c57f58e9594c479cb24
Osama Mustafa <O.mustafaoracle@yahoo.com>
2020-04-18 19:05:46 +0300

    Create Function_Example_2.py
    the program prints "X spotted!". As earlier, if the user inputs "quit", the program terminates.

---
+++ Fuctions/Function_Example_2.py
@@ -0,0 +1,17 @@
+def main():
+    while True:
+        userinput = input("Write something (quit ends): ")
+        if(userinput == "quit"):
+            break
+        else:
+            tester(userinput)
+
+def tester(givenstring="Too short"):
+    if(len(givenstring) < 10):
+        print("Too short")
+    else:
+        print(givenstring)
+
+
+if __name__ == "__main__":
+    main()
